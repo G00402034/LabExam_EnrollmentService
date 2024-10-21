ccf79271840f3042e602eb20872a34371dedd50c
G00402034 <G00402034@atu.ie>
2024-10-21 12:51:07 +0100

    initial commit

---
+++ src/main/java/ie/atu/lab_exam_ms2/LabExamMs2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_exam_ms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamMs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamMs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab_exam_ms2/LabExamMs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab_exam_ms2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamMs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
